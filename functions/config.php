4f9bfe461aad80ec78146a1f790b8fdc6949904d
VidKein <landdru@ukr.net>
2023-05-09 17:19:45 +0300

    htaccess fail

--- functions/config.php
+++ functions/config.php
@@ -1,7 +1,7 @@
 <?php
     define('DB_SERVER', '127.0.0.1:3306');
-    define('DB_USERNAME', 'root');/*Имя - mysql_kv*/
-    define('DB_PASSWORD', '');/*Пароль - hxCV8nCWflohJPFJ*/
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', '');
     define('DB_DATABASE', 'mysql_kv');
     $con = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
